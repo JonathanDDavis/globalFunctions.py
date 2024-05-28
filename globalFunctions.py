14c62a3ca463277f870894da8bae4613df1088b0
JonathanDDavis <125523206+JonathanDDavis@users.noreply.github.com>
2024-05-28 13:59:38 -0500

    Add files via upload

---
+++ globalFunctions.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      jd0919889
+#
+# Created:     22/10/2019
+# Copyright:   (c) jd0919889 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+def greeting(name):
+    print("Hello, " + name + ".")
+
+def printScore(score):
+    print("Your score is : ", score)
+
+def helloWorld():
+    print("Hello, World!")
